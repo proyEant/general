afc6ca05a55a786b5ba78678010d1bff77279d9e
Bruno <bruno.trezza@gmail.com>
2020-05-28 16:44:45 -0300

    Nuevo script solo con carga de RData y ejecución de Shiny

---
+++ Shiny TP.r
@@ -0,0 +1,17 @@
+rm(list=ls())
+
+#Setear WD a utilizar
+
+setwd()
+
+#Descargar RData al WD que se quiera utilizar
+
+RData <-'https://drive.google.com/file/d/1NOJAox0zQ98TuGfDQ4t5BIbcgUuKFSTN/view?usp=sharing'
+
+#Cargar RData
+
+load(paste0(getwd(),'/TP-EANT-Covid-19.RData'))
+
+#Ejecución Shiny TP
+
+shinyApp(ui=ui, server=server)
